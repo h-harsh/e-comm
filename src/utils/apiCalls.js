be8f808dddc25aab3daccc39c757ee22d84b44d7
h-harsh <porwalharshp@gmail.com>
2021-09-28 01:26:33 +0530

    server hosting updated to repl

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,2 +1,3 @@
-export const baseurl = "https://fin-store.herokuapp.com"
+// export const baseurl = "https://fin-store.herokuapp.com"
 // export const baseurl = "http://127.0.0.1:5000"
+export const baseurl = "https://server-fin-store.harshporwal1.repl.co"
